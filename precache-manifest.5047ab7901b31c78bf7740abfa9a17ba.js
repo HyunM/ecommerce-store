cf2a52c8851fcd968b2f163e0dd63e32cf918943
HyunM <wisek0226@gmail.com>
2020-07-06 16:47:40 -0700

    Updates

---
+++ precache-manifest.5047ab7901b31c78bf7740abfa9a17ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f1b16b8c4c0ebbbb2aa7a1cc983877b",
+    "url": "/ecommerce-store/index.html"
+  },
+  {
+    "revision": "6273e8a6bfadc047e667",
+    "url": "/ecommerce-store/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6e6145b41511da2f5769",
+    "url": "/ecommerce-store/static/css/main.3e158934.chunk.css"
+  },
+  {
+    "revision": "6273e8a6bfadc047e667",
+    "url": "/ecommerce-store/static/js/2.7430590c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ecommerce-store/static/js/2.7430590c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6145b41511da2f5769",
+    "url": "/ecommerce-store/static/js/main.f6e0a6bf.chunk.js"
+  },
+  {
+    "revision": "3d5f7af9f0abfcc2c548",
+    "url": "/ecommerce-store/static/js/runtime-main.570ce59b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/ecommerce-store/static/media/logo.9f7e506d.svg"
+  }
+]);
